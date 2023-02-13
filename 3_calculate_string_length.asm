89b63a959c72d61d88c2ce3dc1d6e9882673cb6d
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-02-13 20:28:02 +0530

    3 updated

---
+++ 3_calculate_string_length.asm
@@ -0,0 +1,29 @@
+section .data
+    msg db "Hello world Vaishnavi!", 0Ah
+    msgLen equ $-msg
+
+section .text
+    global _start
+_start:
+    mov ebx,msg
+    mov eax,ebx
+
+    nextChar:
+        cmp byte[eax],0
+        jz finished
+        inc eax
+        jmp nextChar
+
+    finished:
+        sub eax,ebx
+
+        mov eax,4
+        mov ebx,1
+        mov ecx,msg
+        mov edx,eax
+        int 80h
+
+    mov eax,1
+    mov ebx,0
+    int 80h
+    
