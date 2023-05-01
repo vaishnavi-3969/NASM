b152ea00bcd50bc9ae4109816b0459581aa35aa0
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-05-02 00:10:08 +0530

    completed

--- final_practice/arithmetic&logical.asm
+++ final_practice/arithmetic&logical.asm
@@ -4,7 +4,7 @@ section .data
     len_num1 equ $-msg_num1
     msg_num2 db "Enter number 2: ",0
     len_num2 equ $-msg_num2
-    msg_menu db 10,"!===============Menu===============!",10,"---------ARITHMETIC OPERATIONS---------",10,"1. Addition",10,"2. Subtraction",10,"3. Multiplication",10,"4. Division",10,"---------LOGICAL OPERATIONS---------",10,"5. AND",10,"6. OR",10,"7. NOT",10,"8. NAND",10,"9. NOR",10,"10. XNOR",10,"0. Exit",10,"Enter your choice",10,0
+    msg_menu db 10,"!===============Menu===============!",10,"1. Addition",10,"2. Subtraction",10,"3. Multiplication",10,"4. Division",10,"5. AND",10,"6. OR",10,"7. NOT",10"0. Exit",10,"Enter your choice",10,0
     len_menu equ $-msg_menu
     msg_result db "The result is: ", 0
     len_result equ $ - msg_result
@@ -16,7 +16,7 @@ section .data
     len_sub equ $-msg_sub
     msg_mul db "---------Multiplication---------",10,0
     len_mul equ $-msg_mul
-    msg_div db "---------Division---------",10,0
+    msg_div db "---------Division---------",1,0
     len_div equ $-msg_div
     msg_and db "---------AND---------",10,0
     len_and equ $-msg_and
@@ -24,12 +24,6 @@ section .data
     len_or equ $-msg_or
     msg_not db "---------NOT---------",10,0
     len_not equ $-msg_not
-    msg_nand db "---------NAND---------",10,0
-    len_nand equ $-msg_nand
-    msg_nor db "---------NOR---------",10,0
-    len_nor equ $-msg_nor
-    msg_xnor db "---------XNOR---------",10,0
-    len_xnor equ $-msg_xnor
     msg_invalid db "INVALID CHOICE",10,0
     len_invalid equ $-msg_invalid
     
@@ -88,12 +82,6 @@ _invalid:
     jmp _start
     
 _input:
-; displaying msg
-    mov eax,4
-    mov ebx,1
-    mov ecx, msg_add
-    mov edx, len_add
-    int 80h
     
     ; displaying message to enter first number
     mov eax,4
@@ -125,6 +113,13 @@ _input:
     ret
     
 _add:
+; displaying msg
+    mov eax,4
+    mov ebx,1
+    mov ecx, msg_add
+    mov edx, len_add
+    int 80h
+    
     call _input
     mov eax, [num1]
     sub eax,'0'
@@ -136,6 +131,12 @@ _add:
     jmp _display
 
 _subtract:
+; displaying msg
+    mov eax,4
+    mov ebx,1
+    mov ecx, msg_sub
+    mov edx, len_sub
+    int 80h
     call _input
     mov eax, [num1]
     sub eax,'0'
@@ -147,6 +148,12 @@ _subtract:
     jmp _display
 
 _multiplication:
+; displaying msg
+    mov eax,4
+    mov ebx,1
+    mov ecx, msg_mul
+    mov edx, len_mul
+    int 80h
    call _input
     ; perform multiplication
     mov al,[num1]
@@ -159,6 +166,12 @@ _multiplication:
     jmp _display
     
 _division:
+; displaying msg
+    mov eax,4
+    mov ebx,1
+    mov ecx, msg_div
+    mov edx, len_div
+    int 80h
     call _input
     ; perform division
     mov al,[num1]
@@ -171,6 +184,12 @@ _division:
     jmp _display
 
 _and:
+; displaying msg
+    mov eax,4
+    mov ebx,1
+    mov ecx, msg_and
+    mov edx, len_and
+    int 80h
     call _input
     ; performing operations
     mov eax,[num1]
@@ -183,6 +202,12 @@ _and:
     jmp _display
     
 _or:
+; displaying msg
+    mov eax,4
+    mov ebx,1
+    mov ecx, msg_or
+    mov edx, len_or
+    int 80h
     call _input
     ; performing operations
     mov eax,[num1]
@@ -193,35 +218,30 @@ _or:
     add eax,'0'
     mov [result],eax
     jmp _display
-    
+
 _not:
-    mov eax,4
-    mov ebx,1
+    ; display message
+    mov eax, 4
+    mov ebx, 1
     mov ecx, msg_not
     mov edx, len_not
     int 80h
     
-    mov eax,4
-    mov ebx,1
-    mov ecx, msg_num1
-    mov edx, msg_num2
-    int 80h
-    ; inputing a no.
-    mov eax,3
-    mov ebx,0
+    ; read input
+    mov eax, 3
+    mov ebx, 0
     mov ecx, num1
     mov edx, 2
     int 80h
     
-    mov eax,[num1]
-    sub eax,'0'
+    ; perform logical NOT
+    mov eax, [num1]
     not eax
-    add eax,'0'
-    mov [result],eax
+    mov [result], eax
     
-    ; displaying result
-    mov eax,4
-    mov ebx,1
+    ; display result
+    mov eax, 4
+    mov ebx, 1
     mov ecx, msg_result
     mov edx, len_result
     int 80h
@@ -232,7 +252,7 @@ _not:
     mov edx, 2
     int 80h
     jmp _start
-    
+
 _display:
     ; displaying msg of result
     mov eax,4
