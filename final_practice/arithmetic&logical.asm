7f57a86ef91e9180552b30b8ea7d43c5cf3f56b0
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-05-01 16:58:04 +0530

    updated

--- final_practice/arithmetic&logical.asm
+++ final_practice/arithmetic&logical.asm
@@ -1,231 +1,85 @@
+; performing all the arithmetic operations
 section .data
-; performing arithmetic and logical operations for single digit
-   msg1 db 'Enter the num1',0xa,0xd
-   len1 equ $-msg1
-   msg2 db 'Enter the num2',0xa,0xd
-   len2 equ $-msg2
-   msg3 db 'Addition of number :',0xa,0xd
-   len3 equ $-msg3
-   msg4 db 'Subtraction of number :',0xa,0xd
-   len4 equ $-msg4
-   msg5 db 'Multiplication of the number',0xa,0xd
-   len5 equ $-msg5
-   msg6 db 'Division of number :',0xa,0xd
-   len6 equ $-msg6
-   msg7 db 'Result of OR operation: ',0xa,0xd
-   len7 equ $-msg7
-   msg8 db 'Result of AND operation: ',0xa,0xd
-   len8 equ $-msg8
-   msg9 db 'Result of XOR operation: ',0xa,0xd
-   len9 equ $-msg9
-   msg10 db 'Result of XOR operation: ',0xa,0xd
-   len10 equ $-msg10
-
-
-
-segment .bss
-
-   num1 resb 2
-   num2 resb 2
-   sum  resb 1
-   len equ $-sum
-   subt resb 1
-   divi resb 1
-   multi resb 1
-   lenm equ $-sum
-   result resb 1
-
-
+    msg_num1 db "Enter number 1: ",10,0
+    len_num1 equ $-msg_num1
+    msg_num2 db "Enter number 2: ", 10,0
+    len_num2 equ $-msg_num2
+    msg_sum db "1. Addition",10,0
+    len_sum equ $-msg_sum
+    msg_diff db "2. Subtraction",10,0
+    len_diff equ $-msg_diff
+    msg_exit db "0. Exit", 10,0
+    len_exit equ $-msg_exit
+    msg_menu db "-------------Menu-------------",10,0
+    len_menu equ $-msg_menu
+    msg_mul db "3. Multiplication",10,0
+    len_mul equ $-msg_mul
+    msg_div db "4. Division",10,0
+    len_div equ $-msg_div
+    msg_choice db "Enter your choice: ",10,0
+    len
+    
+section .bss
+    num1 resb 2
+    num2 resb 2
+    addition resb 2
+    subtraction resb 2
+    multiply resb 2
+    division resb 2
+    
 section .text
-   global _start       ;must be declared for using gcc
-_start:                    ;tell linker entry point
-
-
-   ; display message to enter the msg1
-   mov eax,4       
-   mov ebx,1
-   mov ecx,msg1
-   mov edx,len1
-   int 80h
-
-   ; to accept the string 1
-   mov eax,3           
-   mov ebx,0
-   mov ecx,num1
-   mov edx,2
-   int 80h
-
-  ; display message to enter the string2
-   mov eax,4         
-   mov ebx,1
-   mov ecx,msg2
-   mov edx,len2
-   int 80h
-
-  ; to accept the string 2
-   mov eax,3          
-   mov ebx,0
-   mov ecx,num2
-   mov edx,2
-   int 80h
-   
-   mov eax,[num1]
-   sub eax,'0'
-   mov ebx,[num2]
-   ; subtract to convert ascii into decimal
-   sub ebx,'0'
-   add eax,ebx
-   ; to convert decimal into ascii again
-   add eax,'0'
-   mov [sum],eax
-
-   ; to display msg of sum op
-   mov eax,4
-   mov ebx,1
-   mov ecx,msg3
-   mov edx,len3
-   int 80h
-   
-   ; to display sum op
-   mov eax,4
-   mov ebx,1
-   mov ecx,sum
-   mov edx,len
-   int 80h
-
-   mov eax,[num1]
-   ; subtract to convert ascii into decimal
-   sub eax,'0'
-   mov ebx,[num2]
-   sub ebx,'0'
-   sub eax,ebx
-   ; to convert decimal into ascii again
-   add eax,'0'
-   mov [subt],eax
-
-    ; to display msg of sub op
-   mov eax,4
-   mov ebx,1
-   mov ecx,msg4
-   mov edx,len4
-   int 80h
-
-   ; to display sub op
-   mov eax,4
-   mov ebx,1
-   mov ecx,subt
-   mov edx,1
-   int 80h  
- 
-   mov eax,[num1]
-   sub eax,'0'
-   mov ebx,[num2]
-   sub ebx,'0'
-   mul ebx
-   add eax,'0'
-   mov [multi],eax 
-
-   ; to display msg of mul op
-   mov eax,4
-   mov ebx,1
-   mov ecx,msg5
-   mov edx,len5
-   int 80h
-   
-  ; to display mul op
-   mov eax,4
-   mov ebx,1
-   mov ecx,multi
-   mov edx,1
-   int 80h
-
-   mov al,[num1]
-   sub al,'0'
-   mov bl,[num2]
-   sub bl,'0'
-   div bl
-   add al,'0'
-   mov [divi],al
-
-   ; to display msg of divi op
-   mov eax,4
-   mov ebx,1
-   mov ecx,msg6
-   mov edx,len6
-   int 80h
-   
-   ; to display divi op
-   mov eax,4
-   mov ebx,1
-   mov ecx,divi
-   mov edx,1
-   int 80h
-
-   ; to display msg of  op
-   mov eax,4
-   mov ebx,1
-   mov ecx,msg7
-   mov edx,len7
-   int 80h
-
-   mov eax,[num1]
-   sub eax,'0'
-   mov ebx,[num2]
-   sub ebx,'0'
-   OR eax, ebx
-   add eax,'0'
-   mov [result], eax
-
-   ; to display op
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, result
-   mov edx, 2
-   int 80h
- 
-    mov eax, [num1]
-   mov ebx, [num2]
-   and eax, ebx
-   mov [result], eax
-
-   ; to display msg of op
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, msg8
-   mov edx, len8
-   int 0x80
-
-   ; to display op
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, result
-   mov edx, 2
-   int 0x80
-
-   ; to display msg of op
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, msg9
-   mov edx, len9
-   int 0x80
-
-   mov eax,[num1]
-   sub eax,'0'
-   mov ebx,[num2]
-   sub ebx,'0'
-   XOR eax,ebx
-   add eax,'0'
-
-   mov [result], eax
-
-   ; to display op
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, result
-   mov edx, 2
-   int 80h
-
-   mov eax,1
-   mov ebx,0
-   int 80h
-
+    global _start
+    
+_start:
+    call _menu
+    
+_menu:
+    ; menu
+    ; display menu option
+    mov eax,4
+    mov ebx,1
+    mov ecx, msg_menu
+    mov edx, len_menu
+    int 80h
+    
+    ; display sum option
+    mov eax,4
+    mov ebx,1
+    mov ecx, msg_sum
+    mov edx, len_sum
+    int 80h
+    
+    ; display diff option
+    mov eax,4
+    mov ebx,1
+    mov ecx, msg_diff
+    mov edx, len_diff
+    int 80h
+    
+    ; display multiplication option
+    mov eax,4
+    mov ebx,1
+    mov ecx, msg_mul
+    mov edx, len_mul
+    int 80h
+    
+    ; display division option
+    mov eax,4
+    mov ebx,1
+    mov ecx, msg_div
+    mov edx, len_div
+    int 80h
+    
+    ; display exit option
+    mov eax,4
+    mov ebx,1
+    mov ecx, msg_exit
+    mov edx, len_exit
+    int 80h
+    ; display to enter the user's choice
+    
+_exit:  
+    mov eax,1
+    mov ebx,0
+    int 80h
+    
