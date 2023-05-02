c15525ff7dc350da9b679565a23fa5ef036ef288
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-05-02 12:34:55 +0530

    refactor

--- final_practice/length.asm
+++ final_practice/length.asm
@@ -2,15 +2,14 @@
     mov eax,4
     mov ebx,1
     mov ecx, %1
-    mov edx, %2
+    mov edx,%2
     int 80h
 %endmacro
-
 %macro read 2
     mov eax,3
     mov ebx,0
     mov ecx, %1
-    mov edx, %2
+    mov edx,%2
     int 80h
 %endmacro
 
@@ -19,19 +18,19 @@ section .bss
     quotient resb 2
 
 section .data
-    msg1 db "Enter the string: ",10,0
-    len1 equ $-msg1
-    msg2 db "Length of the string: ",10,0
-    len2 equ $-msg2
+    msg_string db "Enter string: ",10,0
+    len_msg_string equ $-msg_string
+    msg_length db "The length of the string: ",10,0
+    len_msg_length equ $-msg_length
     
 section .text
 global _start
 _start:
-    display msg1,len1
+    display msg_string, len_msg_string
     read str1,100
-    jmp len
-
-len:
+    jmp length
+    
+length:
     dec al
     mov bl,10
     div bl
@@ -39,12 +38,12 @@ len:
     add ah,30h
     mov [quotient+1],ah
     mov [quotient],al
-    display msg2,len2
+    display msg_length, len_msg_length
     display quotient,2
     int 80h
     jmp exit
-exit: 
+
+exit:
     mov eax,1
     mov ebx,0
     int 80h
-    
