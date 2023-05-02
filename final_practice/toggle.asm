2fe8f4a1b15bf5ed83083a272eab002e08194090
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-05-02 14:03:23 +0530

    toggle

---
+++ final_practice/toggle.asm
@@ -0,0 +1,85 @@
+;Toggle 
+%macro display 2
+    mov eax,4
+    mov ebx,1
+    mov ecx,%1
+    mov edx,%2
+    int 80h
+%endmacro
+
+%macro read 2
+    mov eax,3
+    mov ebx,0
+    mov ecx,%1
+    mov edx,%2
+    int 80h
+%endmacro
+
+section .bss
+    string resb 10
+    tog resb 10
+
+section .data
+    msg1 db "Enter the string: ",10,0
+    len1 equ $-msg1
+    msg2 db "Toggle: ",10,0
+    len2 equ $-msg2
+
+section .text
+global _start
+_start:
+  display msg1,len1
+  read string,10
+  jmp toggle
+  
+toggle:
+    dec al
+    mov esi,string
+    mov edi,tog
+    mov cl,al
+    jmp _toggle
+
+_toggle:
+    mov al,byte[esi]
+    cmp al,'a'
+    jb _store
+    cmp al,'a'
+    jae lowercase
+    
+lowercase:
+    cmp al,'z'
+    jb _lowercase
+    cmp al,'z'
+    ja uppercase
+    
+_lowercase:
+    sub al, ' '
+    jmp _store
+   
+uppercase:
+    cmp al,'A'
+    jb _store
+    cmp al,'A'
+    jae _uppercase
+
+_uppercase:
+    cmp al,'Z'
+    ja _store
+    add al,' '
+    jmp _store
+
+_store:
+    mov byte[edi],al
+    inc edi,
+    inc esi
+    dec cl
+    jnz _toggle
+    display msg2,len2
+    display tog,10
+    jmp exit
+         
+exit:
+    mov eax,1
+    mov ebx,0
+    int 80h
+    
